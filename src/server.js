d9ee98a3823c70a3adfc7bc8f0ef38b26bb5608a
Nizam <ramazanov570633@gmail.com>
2023-11-11 18:43:06 +0400

    Fix3

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
-const http =require('http')
+const https =require('https')
 const app =require('./app')
 require('dotenv').config();
-const server = http.createServer(app)
+const server = https.createServer(app)
 
   
 server.listen(8080)
